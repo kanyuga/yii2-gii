f5194c5ca3bf24deae50d1d566257eff8f23224f
kanyuga <kanjoti@gmail.com>
2015-09-08 22:51:11 +0300

    Changed forms to yii\bootstrap\ActiveForm

--- generators/crud/default/views/_form.php
+++ generators/crud/default/views/_form.php
@@ -17,11 +17,11 @@ echo "<?php\n";
 ?>
 
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
+use yii\bootstrap\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model <?= ltrim($generator->modelClass, '\\') ?> */
-/* @var $form yii\widgets\ActiveForm */
+/* @var $form yii\bootstrap\ActiveForm */
 ?>
 
 <div class="<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>-form">
